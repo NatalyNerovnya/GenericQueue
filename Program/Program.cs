497fc8308aecf313b260cb525af225d430b0835a
Наташа Неровня <natanerovnja@gmail.com>
2016-02-07 21:15:39 +0300

    add console application

---
+++ Program/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomQueue;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomQueue<object> queue = new CustomQueue<object>();
+            queue.Enqueue("My name is Nataly");
+            queue.Enqueue("Something else");
+            queue.Enqueue(312);
+            CustomQueue<object> queue2 = queue.Clone();
+            CustomQueue<object> queue3 = queue;
+            queue.Enqueue(new List<int>());
+            queue.Enqueue(new DateTime());
+            foreach (var variable in queue)
+            {
+                Console.WriteLine(variable);
+            }
+            Console.WriteLine('\n');
+            foreach (var variable in queue2)
+            {
+                Console.WriteLine(variable);
+            }
+            Console.WriteLine('\n');
+            foreach (var variable in queue3)
+            {
+                Console.WriteLine(variable);
+            }
+            Console.WriteLine('\n');
+            Console.ReadLine();
+        }
+    }
+}
